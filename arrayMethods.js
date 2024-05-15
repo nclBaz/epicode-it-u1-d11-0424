3c2b4956769ea0a192b532cfe233f2c5789b20c0
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-05-15 09:38:00 +0200

    Adds reduce example

--- arrayMethods.js
+++ arrayMethods.js
@@ -32,6 +32,15 @@ for (let index = 0; index < animals2.length; index++) {
 }
 
 console.log("Con FOREACH e funzioni normali")
+
+// const stampaAnimale = function (animal) {
+//   console.log(animal)
+// }
+
+// animals.forEach(stampaAnimale)
+
+animals.forEach(animal => console.log(animal))
+
 animals2.forEach(function (currentAnimal) {
   // For Each consente di eseguire una certa funzione elemento per elemento, quindi può essere una buona alternativa ad un for loop classico, più semplice anche da leggere
   // Il For Each prende come parametro una FUNZIONE, il cui primo parametro sarà SEMPRE l'elemento corrente (LO POSSO CHIAMARE COME VOGLIO)
@@ -49,6 +58,7 @@ const numbers = [1, 2, 3, 4, 5, "6", 7, 8, 9, 10]
 //   const element = numbers[index]
 //   quadrati.push(element * element)
 // }
+
 const quadrati = numbers.map(function (number) {
   const quadrato = number * number
   return { quadrato: quadrato, numeroOriginale: number } // Il return decide cosa sto ritornando, posso tornare numeri, stringhe, oggetti, array,........
@@ -64,6 +74,10 @@ const quadrati2 = numbers.map(number => {
 
 // // -------------------------------- FILTER ------------------------------
 // Il FILTER è un metodo che consente di filtrare elementi di un array, restituendoci quindi un NUOVO ARRAY con (probabilmente) meno elementi di prima. Il procedimento si basa sul andare ad impostare una condizione TRUE/FALSE nella clausola di RETURN della funzione interna
+numbers.filter(function (number) {
+  return number > 3 && number < 5
+})
+
 const filteredNumbers = numbers.filter(function (number) {
   return number > 4 // All'interno del filter devo specificare nel return la condizione per la quale l'elemento dovrà essere filtrato o meno
 })
@@ -72,3 +86,148 @@ console.log(filteredNumbers) // Ho ottenuto un nuovo array con meno elementi di 
 
 // console.log(filteredNumbers.map(num => num * num))
 // console.log(numbers.filter(num => num > 4).map(num => num * num))
+
+// ************************************ REDUCE **************************************
+
+// Il reduce è un metodo degli array che consente di compattare un array in un unico valore, applicando un qualche tipo di calcolo
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// ES: voglio calcolare la somma dell'array numbers2
+const sum = numbers2.reduce(function (accumulator, currentElem) {
+  // I ITERATION: accumulator = 0, currentElem = 1 --> accumulator = accumulator + currentElem, cioè 0+1=1
+  // II ITERATION: accumulator = 1, currentElem = 2 --> accumulator = accumulator + currentElem, cioè 1+2=3
+  // III ITERATION: accumulator = 3, currentElem = 3 --> accumulator = accumulator + currentElem, cioè 3+3=6
+  // IV ITERATION: accumulator = 6, currentElem = 4 --> accumulator = accumulator + currentElem, cioè 6+4=10
+  return accumulator + currentElem
+}, 0) // questo zero indica quale sarà il valore di partenza dell'accumulator
+
+console.log("La somma è:", sum)
+
+const prod = numbers2.reduce(function (accumulator, currentElem) {
+  return accumulator * currentElem
+}, 1)
+console.log("Il prodotto è:", prod)
+
+const users = [
+  { name: "Aldo", age: 20 },
+  { name: "Giovanni", age: 30 },
+  { name: "Giacomo", age: 40 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+  { name: "Ajeje", age: 50 },
+]
+
+// const sum2 = users
+//   .filter(function (user) {
+//     return user.age > 30
+//   })
+//   .map(function (user) {
+//     return user.age
+//   })
+//   .reduce(function (acc, curr) {
+//     return acc + curr
+//   }, 0)
+
+const sum2 = users
+  .filter(user => user.age > 30)
+  .map(user => user.age)
+  .reduce((acc, curr) => acc + curr, 0)
+
+console.log("sum2 ", sum2)
